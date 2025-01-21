83027a890096423b39e36bc84ec0d8425e93bb11
Nir Goren <nir@gorenor.com>
2025-01-21 18:19:08 +0200

    Update run all

--- run_all.py
+++ run_all.py
@@ -3,9 +3,15 @@ from pathlib import Path
 import subprocess
 
 def run_all(run_yaml_dir, data_yaml_dir, output_base_dir):
-    # Get all YAML files in the specified directories (recursive search)
-    run_yaml_files = list(run_yaml_dir.rglob("*.yaml"))
-    data_yaml_files = list(data_yaml_dir.rglob("*.yaml"))
+    if run_yaml_dir.is_file():
+        run_yaml_files = [run_yaml_dir]
+    else:
+        # Get all YAML files in the specified directories (recursive search)
+        run_yaml_files = list(run_yaml_dir.rglob("*.yaml"))
+    if data_yaml_dir.is_file():
+        data_yaml_files = [data_yaml_dir]
+    else:
+        data_yaml_files = list(data_yaml_dir.rglob("*.yaml"))
 
     # Iterate through all combinations of run_yaml and data_yaml
     for run_yaml_path in run_yaml_files:
@@ -37,3 +43,4 @@ if __name__ == "__main__":
     parser.add_argument("--output_base_dir", type=Path, required=True, help="Base directory for output")
     args = parser.parse_args()
     run_all(args.run_yaml_dir, args.data_yaml_dir, args.output_base_dir)
+    print("All combinations completed")
